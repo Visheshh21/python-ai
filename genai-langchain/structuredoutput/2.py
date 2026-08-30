d6238fdea5c9b9cc8c8ca1ac3c64eb9bd542bc6c
Vishesh Goel <goelvishu13@gmail.com>
2026-08-30 23:29:41 +0530

    learning: structuredoutput using with_structured_output compatibility

---
+++ genai-langchain/structuredoutput/2.py
@@ -0,0 +1,34 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Optional, Literal
+from pydantic import BaseModel, EmailStr, Field
+
+load_dotenv()
+
+model = ChatGroq(model_name='openai/gpt-oss-20b',temperature=0.5,max_tokens=None)
+
+class review(BaseModel):
+    summary: str=Field(description="A brief Summary of the review under 100 words")
+    sentiment: Literal["Positive", "Negative"] = Field(description="Sentiment of the review (positive/negative)")
+    rating: float = Field(ge=1, le=5, description="Rating of the product (1-5)") 
+    pros: Optional[list[str]]=Field(description="Write down all the pros inside a list")
+    cons: Optional[list[str]]=Field(description="Write down all the cons inside a list, If they are explicitly mentioned in the review")
+
+structured_model=model.with_structured_output(review)
+
+result=structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast–whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera–the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware–why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+""")
+
+print(result)
+ 
